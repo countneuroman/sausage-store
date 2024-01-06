408df30cbeccacb16f6d11dcfc901583af5ab6b2
CountNeuroman <olorinoren@gmail.com>
2024-01-06 12:17:16 +0300

    Берем статику из нашего s3

---
+++ backend/src/main/resources/db/migration/V004__update_product_picture_url.sql
@@ -0,0 +1,24 @@
+﻿UPDATE product
+SET picture_url = 'https://static-std-023-61.website.yandexcloud.net/creamy_sausage.jpg' 
+where id = 1;
+
+UPDATE product
+SET picture_url = 'https://static-std-023-61.website.yandexcloud.net/special_sausage.jpg'
+where id = 2;
+
+UPDATE product
+SET picture_url = 'https://static-std-023-61.website.yandexcloud.net/dairy_sausage.jpg'
+where id = 3;
+
+UPDATE product
+SET picture_url = 'https://static-std-023-61.website.yandexcloud.net/nuremberg_sausage.jpg'
+where id = 4;
+
+
+UPDATE product
+SET picture_url = 'https://static-std-023-61.website.yandexcloud.net/munich_sausage.jpg'
+where id = 5;
+
+UPDATE product
+SET picture_url = 'https://static-std-023-61.website.yandexcloud.net/russian_sausage.jpg'
+where id = 6;
